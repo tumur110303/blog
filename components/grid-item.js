078f8f62a07d3708bf9eaff02985fae9422b76e4
tumuruu <mbt.isee@gmail.com>
2021-11-11 15:21:08 +0800

    publisher tei bolgoson

--- components/grid-item.js
+++ components/grid-item.js
@@ -1,12 +1,13 @@
 import { Card } from "react-bootstrap";
+import Link from "next/link";
 
-export default () => {
+export default ({ post }) => {
   return (
     <Card className={`fj-card`}>
       <div className="card-body-wrapper">
         <Card.Header className="d-flex flex-row">
           <img
-            src={"https://source.unsplash.com/user/erondu/150x150"}
+            src={post.publisher.picture}
             className="rounded-circle mr-3"
             height="50px"
             width="50px"
@@ -14,23 +15,22 @@ export default () => {
           />
           <div>
             <Card.Title className="font-weight-bold mb-1">
-              Амар Өсөхбаяр
+              {post.publisher.title}
             </Card.Title>
-            <Card.Text className="card-date">2021 оны 3 сарын 2</Card.Text>
+            <Card.Text className="card-date">{post.date}</Card.Text>
           </div>
         </Card.Header>
-        <div className="view overlay">
-          <Card.Img
-            src="https://source.unsplash.com/user/erondu/250x250"
-            alt="Card image cap"
-          />
-        </div>
-        <Card.Body>
-          <Card.Title className="card-main-title">
-            Микросэрвис орчин үед
-          </Card.Title>
-          <Card.Text>Вэб технологи</Card.Text>
-        </Card.Body>
+        <Link href={`/${post.slug}`}>
+          <a>
+            <div className="view overlay">
+              <Card.Img src={post.image} alt="Card image cap" />
+            </div>
+            <Card.Body>
+              <Card.Title className="card-main-title">{post.title}</Card.Title>
+              <Card.Text>{post.subtitle}</Card.Text>
+            </Card.Body>
+          </a>
+        </Link>
       </div>
     </Card>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,12 @@
-import {
-  Container,
-  Row,
-  Col,
-  Navbar,
-  Nav,
-  Media,
-  Image,
-  Card,
-} from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 import MyNavbar from "components/my-navbar";
 import Intro from "components/intro";
 import ListItem from "components/list-item";
 import GridItem from "components/grid-item";
+import { getAllPosts } from "lib/api";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <Container>
       <MyNavbar />
@@ -25,37 +17,21 @@ export default function Home() {
           </Col>
         </Row>
 
+        <pre>{JSON.stringify(posts, null, 2)}</pre>
+
         <hr />
 
         <div className={`page-wrapper`}>
           <Row className="mb-5">
-            <Col md="10">
+            {/* <Col md="10">
               <ListItem />
-            </Col>
+            </Col> */}
 
-            <Col md="4">
-              <GridItem />
-            </Col>
-
-            <Col md="4">
-              <GridItem />
-            </Col>
-
-            <Col md="4">
-              <GridItem />
-            </Col>
-
-            <Col md="4">
-              <GridItem />
-            </Col>
-
-            <Col md="4">
-              <GridItem />
-            </Col>
-
-            <Col md="4">
-              <GridItem />
-            </Col>
+            {posts.map((post) => (
+              <Col md="4">
+                <GridItem post={post} />
+              </Col>
+            ))}
           </Row>
         </div>
       </div>
@@ -71,3 +47,13 @@ export default function Home() {
     </Container>
   );
 }
+
+export const getStaticProps = async () => {
+  const posts = await getAllPosts();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
